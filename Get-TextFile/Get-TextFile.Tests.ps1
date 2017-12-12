7926ec7c897f59ae5b800131ce26d46156cc92e3
Bazza <barry.goodall@os.uk>
2017-12-12 17:25:15 +0000

    Added some examples

--- Get-TextFile/Get-TextFile.Tests.ps1
+++ Get-TextFile/Get-TextFile.Tests.ps1
@@ -7,8 +7,26 @@ function CreateFileList([string[]]$names){
         [PSCustomObject]@{ FullName = "c:\foo\bar\$_"; Name = $_; }
     }
  
-}  
+}
 
+<#
+Describe 'Get-TextFileNames' {
+    It 'returns two text files' {
+        Get-TextFileNames | Should Be ('TestFileInFolder01.txt','TestFileInFolder03.txt')
+    }
+}
+#>
+
+<#
+Describe 'Get-TextFileNames' {
+	    It 'returns one text file' {
+	        Mock Get-ChildItem { [PSCustomObject]@{ Name = 'a923e023.txt' } }
+	        Get-TextFileNames | Should Be 'a923e023.txt'
+	    }
+    }
+
+#>
+<#
 Describe 'Get-TextFileNames' {
     
        It 'returns one text file when that is all there is' {
@@ -35,4 +53,41 @@ Describe 'Get-TextFileNames' {
            Get-TextFileNames | Should BeNullOrEmpty
        }
     
-   } 
+   }
+#>
+
+<#
+Describe 'Get-TextFileNames' {
+    
+     ContextUsing "file combinations" (
+    
+       # Data for test #1
+       ('a923e023.txt', # the $files parameter
+        'a923e023.txt', # the $expectedResult parameter
+        'one text file when that is all there is'), # the description parameter
+    
+       # Data for test #2
+       (('a923e023.txt','wlke93jw3.doc'),
+        'a923e023.txt',
+        'one text file when there are assorted files'),
+    
+       # Data for test #3
+       (('a923e023.txt','wlke93jw3.doc','ke923jd.txt','qq02000.doc'),
+        ('a923e023.txt','ke923jd.txt'),
+        'multiple text files amongst assorted files'),
+    
+       # Data for test #4
+       (('wlke93jw3.doc','qq02000.doc'),
+        $null,
+        'nothing when there are no text files')
+    
+     ) {
+       param($files, $expectedResult, $description)
+    
+          It "returns $description" {
+             Mock Get-ChildItem { CreateFileList $files }
+             Get-TextFileNames | Should Be $expectedResult
+         }
+     }
+    }
+#>
--- Get-TextFile/Get-TextFile.ps1
+++ Get-TextFile/Get-TextFile.ps1
@@ -1,3 +1,13 @@
+
 function Get-TextFileNames() {
-    Get-ChildItem | Where-Object Name -like *.txt | Select-Object -expand Name
+
+    $path = "C:\Users\bgoodall\azure\pester-scratch\Get-TextFile\"
+
+    Write-Host "About to get text files"
+
+    Get-ChildItem  -Path $path| Where-Object Name -like *.txt | Select-Object -expand Name
+
+    Write-Host "Function is finished looking for text files."
 }  
+
+$test = Get-TextFileNames
